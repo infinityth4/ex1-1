c7e8cd14df186dbe3f671fcc0e02362cacebe5ea
Hiroshi Tashiro <infinith4@gmail.com>
2012-08-08 06:25:25 +0900

    Update main.pl

--- main.pl
+++ main.pl
@@ -3,8 +3,7 @@ use strict;
 use warnings;
 use TemplateEngine;
 
-my $template = TemplateEngine->new( file => 'templates/main.html' );#->はnewメソッドの呼び出し。=>は、ハッシュ（初めてのPerl,p121）（キーと値を結びつける）
-
+my $template = TemplateEngine->new( file => 'templates/main.html' );
 print $template->render({
   title   => 'タイトル',
   content => 'これはコンテンツです',
